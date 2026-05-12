12adea8a275c5256e7e55f41dbb32c47181d7234
mafutka <mafutka@gmail.com>
2026-05-12 16:00:15 +0300

    list-modified

--- src/app/components/todo-list/todo-list.ts
+++ src/app/components/todo-list/todo-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Todo } from '../../models/todo.model';
 import { TodoInput } from '../todo-input/todo-input';
 import { TodoService } from '../../services/services';
@@ -6,69 +7,71 @@ import { TodoService } from '../../services/services';
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [TodoInput],
+  imports: [TodoInput, FormsModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.scss',
 })
 export class TodoList {
-  constructor(private todoService: TodoService) {
 
-  }
-  ngOnInit() {
-  this.todoService.getTodos().subscribe((data: any) => {
-    this.todos = data.slice(0, 10); 
-  });
-}
   todos: Todo[] = [];
 
-  addTodo(todo: any) {
-  this.todoService.addTodo(todo).subscribe((newTodo) => {
-    this.todos.push({
-      ...newTodo,
-      completed: false
+  filter: 'all' | 'completed' | 'active' = 'all';
+
+  editingId: number | null = null;
+  editTitle = '';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe((data: any) => {
+      this.todos = data.slice(0, 10);
     });
-  });
-}
+  }
+
+  addTodo(todo: any) {
+    this.todoService.addTodo(todo).subscribe((newTodo) => {
+      this.todos.push({
+        ...newTodo,
+        completed: false
+      });
+    });
+  }
 
   toggle(todo: Todo) {
     todo.completed = !todo.completed;
+    this.todoService.updateTodo(todo).subscribe();
   }
 
   delete(id: number) {
-  this.todoService.deleteTodo(id).subscribe(() => {
-    this.todos = this.todos.filter(t => t.id !== id);
-  });
-}
-
-sortByPriority() {
-  const order = { high: 3, medium: 2, low: 1 };
-
-  this.todos.sort((a, b) => order[b.priority] - order[a.priority]);
-}
-
-filter: 'all' | 'completed' | 'active' = 'all';
-
-get filteredTodos() {
-  if (this.filter === 'completed') {
-    return this.todos.filter(t => t.completed);
+    this.todoService.deleteTodo(id).subscribe(() => {
+      this.todos = this.todos.filter(t => t.id !== id);
+    });
   }
-  if (this.filter === 'active') {
-    return this.todos.filter(t => !t.completed);
+
+  sortByPriority() {
+    const order = { high: 3, medium: 2, low: 1 };
+    this.todos.sort((a, b) => order[b.priority] - order[a.priority]);
   }
-  return this.todos;
-}
-editingId: number | null = null;
-editTitle = '';
 
-startEdit(todo: any) {
-  this.editingId = todo.id;
-  this.editTitle = todo.title;
-}
+  get filteredTodos() {
+    if (this.filter === 'completed') {
+      return this.todos.filter(t => t.completed);
+    }
+    if (this.filter === 'active') {
+      return this.todos.filter(t => !t.completed);
+    }
+    return this.todos;
+  }
 
-saveEdit(todo: any) {
-  todo.title = this.editTitle;
-  this.editingId = null;
+  startEdit(todo: Todo) {
+    this.editingId = todo.id;
+    this.editTitle = todo.title;
+  }
 
-  this.todoService.updateTodo(todo).subscribe();
-}
+  saveEdit(todo: Todo) {
+    todo.title = this.editTitle;
+    this.editingId = null;
+
+    this.todoService.updateTodo(todo).subscribe();
+  }
 }
